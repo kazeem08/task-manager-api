70f34cf29d53f678df7cb50ecd3863ee7870a20e
kazeem <kazeem0825@gmail.com>
2019-06-10 12:37:44 +0100

    Cors added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const config = require('dotenv').config();
+const cors = require('cors');
 require('./db/mongoose');
 
 const userRouter = require('./routers/user');
@@ -9,6 +11,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
